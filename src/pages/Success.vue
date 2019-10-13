1f894163662ac2daaddf7dc82b8d60cc86167365
Sebastian Cardoso <sebastiancardoso92@gmail.com>
2019-10-13 02:10:19 -0300

    Form send success message and come back home

---
+++ src/pages/Success.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container flex justify-center mx-auto">
+    <div class="mt-20">
+      <g-image src="../../static/mail_sent.svg" alt="Mail sent" class="mx-auto" height="300" width="300" />
+      <h2 class="text-2xl text-center font-bold mt-10">¡Tu mensaje fue enviado con éxito!</h2>
+      <g-link to="/">
+        <button class="mx-auto mt-6 block bg-green-700 hover:bg-green-800 text-white text-sm font-semibold tracking-wide uppercase shadow rounded cursor-pointer px-6 py-3">Volver al inicio</button>
+      </g-link>
+    </div>
+  </div>
+</template>
